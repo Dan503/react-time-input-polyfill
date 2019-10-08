93a599d1f63b7bea288419c36f14038b0d8e159e
Daniel Tonon <daniel.tonon@oakton.com.au>
2019-10-08 15:26:49 +1100

    creat-react-app after ejecting
    I needed to eject in order to get the build to output to a "docs" folder in the root. This is so that GitHub Pages will work once enabled.

---
+++ docs/precache-manifest.689267ba3d92594935da35b6b2b55a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0b7ad480c522f7ef8c0a339dfdf27d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1c92eaddd959b3b1715",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "22745cd118f78f8a71e4",
+    "url": "/static/js/2.1cf88e05.chunk.js"
+  },
+  {
+    "revision": "c1c92eaddd959b3b1715",
+    "url": "/static/js/main.0ef34937.chunk.js"
+  },
+  {
+    "revision": "36d4223a475c8d5759e1",
+    "url": "/static/js/runtime-main.22ac4ad1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
