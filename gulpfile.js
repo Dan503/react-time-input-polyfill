7e38b8411ca47f5bcfc6f4d2557718dd78866b32
Daniel Tonon <dan503.npm@gmail.com>
2019-10-12 12:44:22 +1100

    I managed to get a timePolyfillHelpers file to get output into a dist folder

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp')
+var browserify = require('browserify')
+// var babelify = require('babelify')
+var uglify = require('gulp-uglify')
+var buffer = require('vinyl-buffer')
+var source = require('vinyl-source-stream')
+
+gulp.task('browserify', (done) => {
+
+	var b = browserify({
+		entries: './timePolyfillHelpers.js',
+		debug: true,
+		// defining transforms here will avoid crashing your stream
+		transform: ['babelify']
+	});
+
+	return b.bundle()
+		.pipe(source('timePolyfillHelpers.js'))
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist'))
+})
+
+gulp.task('default', gulp.series('browserify'))
