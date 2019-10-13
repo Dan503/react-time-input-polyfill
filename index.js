3b7f5ea08405c50b0f3bff6d87a1a608bf819610
Daniel Tonon <dan503.npm@gmail.com>
2019-10-13 22:31:14 +1100

    removed old unecessary code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import supportsTime from 'time-input-polyfill/core/helpers/supportsTime'
 import loadJS from 'time-input-polyfill/core/helpers/loadJS'
 
 const debugMode = true
+// const debugMode = false
 
 const hasPolyfill = !supportsTime || debugMode
 
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
 				? './timePolyfillHelpers.js'
 				: 'https://cdn.jsdelivr.net/npm/time-input-polyfill/dist/time-input-polyfill.min.js',
 			function() {
-				events.emit('polyfill_loaded', window.TimePolyfill)
+				events.emit('polyfill_loaded', window.timePolyfillHelpers)
 			},
 		)
 	}
@@ -38,53 +39,25 @@ export default class TimeInput extends React.Component {
 		super(props)
 		this.$input = React.createRef()
 		events.on('polyfill_loaded', polyfill => {
-			new polyfill(this.$input.current)
+			this.polyfill = polyfill
 		})
 		this.state = {
-			value: props.value || '',
+			value24hr: props.value || '',
 		}
 	}
 
-	componentDidMount() {
-		if (this.props.value) this.$input.current.value = this.props.value
-	}
+	componentDidMount() {}
 
-	getInputValue() {
-		const $input = this.$input.current
-		return supportsTime ? $input.value : $input.dataset.value
-	}
+	componentDidUpdate(prevProps) {}
 
 	onTimeChange(event) {
 		if (this.props.onChange) {
-			const value = this.getInputValue()
 			this.props.onChange({
-				value,
+				value: this.state.value24hr,
+				value12hr: this.state.value12hr,
 				element: this.$input.current,
 				event,
 			})
-
-			this.setState({ value })
-		}
-	}
-
-	componentDidUpdate(prevProps) {
-		const propsHaveUpdated =
-			prevProps.value !== this.props.value &&
-			this.props.value !== this.state.value
-
-		const $input = this.$input.current
-
-		if (propsHaveUpdated) {
-			$input.value = this.props.value
-			setTimeout(() => this.onTimeChange('props update'), 0)
-		}
-
-		if (!supportsTime) {
-			if ($input.dataset.value !== this.state.value) {
-				this.setState({ value: $input.dataset.value })
-			}
-
-			this.$input.current.polyfill.update()
 		}
 	}
 
@@ -94,17 +67,12 @@ export default class TimeInput extends React.Component {
 			'input',
 			{
 				...props,
-				onBlur: e => {
-					// The only way to get IE to fire the props onChange event without breaking the base polyfill functionality
-					this.onTimeChange(e)
-					if (this.props.onBlur) this.props.onBlur(e)
-				},
-				onChange: e => this.onTimeChange(e), // only works in non-ie browsers
-				// onInput: e => this.onTimeChange(e), // Can't use this because it breaks the selection functionality
+				onInput: () =>
+					!hasPolyfill &&
+					this.setState({ value24hr: this.$input.current.value }),
 				ref: this.$input,
-				type: hasPolyfill ? 'tel' : 'time',
-				'data-state-value': this.state.value,
-				'data-props-value': value,
+				type: hasPolyfill ? 'text' : 'time',
+				value: hasPolyfill ? this.state.value12hr : this.state.value24hr,
 			},
 			null,
 		)
