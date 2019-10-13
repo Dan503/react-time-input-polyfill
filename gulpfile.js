c5750d8f6356bf7d298f2f34657dc83800eeff0a
Daniel Tonon <dan503.npm@gmail.com>
2019-10-13 20:51:51 +1100

    applying prettier auto-formatting

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var uglify = require('gulp-uglify')
 var buffer = require('vinyl-buffer')
 var source = require('vinyl-source-stream')
 
-gulp.task('browserify', (done) => {
-
+gulp.task('browserify', done => {
 	var b = browserify({
 		entries: './timePolyfillHelpers.js',
 		debug: true,
 		// defining transforms here will avoid crashing your stream
-		transform: ['babelify']
-	});
+		transform: ['babelify'],
+	})
 
-	return b.bundle()
+	return b
+		.bundle()
 		.pipe(source('timePolyfillHelpers.js'))
 		.pipe(buffer())
 		.pipe(uglify())
