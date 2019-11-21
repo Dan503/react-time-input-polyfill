57fc980c589d84283c289446d9e6960f3742572a
Daniel Tonon <dan503.npm@gmail.com>
2019-11-21 21:58:37 +1100

    Adding code examples to the demo site

--- test-site/src/App.js
+++ test-site/src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import './App.css'
 import TimeInput from '../../index'
+// import SyntaxHighlighter from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-const ExampleBlock = ({ label, Input }) => {
+console.table(SyntaxHighlighter.supportedLanguages)
+
+const ExampleBlock = ({ label, Input, codeString }) => {
 	const [value, setValue] = useState('20:30')
 
 	return (
 		<div style={{ marginBottom: '2em' }}>
 			<h2>{label} time input</h2>
 
-			<label style={{ display: 'block' }}>
+			<label style={{ display: 'inline-block' }}>
 				<span style={{ marginRight: '0.5em' }}>{label} time input</span>
 				<Input
 					currentValue={value}
@@ -30,16 +35,33 @@ const ExampleBlock = ({ label, Input }) => {
 			<p>
 				{label} returned value: "{value}"
 			</p>
+
+			{!!codeString && (
+				<SyntaxHighlighter
+					style={dark}
+					className="code"
+					language="javascript"
+					showLineNumbers={true}
+				>
+					{codeString.replace(/^\n/, '')}
+				</SyntaxHighlighter>
+			)}
 		</div>
 	)
 }
 
 function App() {
-	let [addedLater, setAddedLater] = useState(false)
-	setTimeout(() => setAddedLater(true), 2000)
+	// let [addedLater, setAddedLater] = useState(false)
+	// setTimeout(() => setAddedLater(true), 2000)
+
 	return (
 		<div className="App">
 			<h1>React Time Input Polyfill</h1>
+
+			<p>
+				Code examples are simplified guides,{' '}
+				<strong>not exact code replicas</strong>.
+			</p>
 
 			<ExampleBlock
 				label="Non-forced polyfill"
@@ -50,6 +72,62 @@ function App() {
 						className={className}
 					/>
 				)}
+				codeString={`
+import React, { useState } from 'react'
+import TimeInputPolyfill from 'react-time-input-polyfill'
+
+const TimeInput = ({ label, currentValue, onInputChange }) => {
+	return (
+		<label>
+			<span>{label}</span>
+			<TimeInputPolyfill
+
+				// set the value through props
+				value={currentValue}
+
+				// onChange will run every time the value is updated
+				onChange={({ value, element }) => {
+					console.log({
+
+						// The current value in 24 hour time format
+						value,
+
+						// The <input> HTML element
+						element,
+
+					})
+
+					// Export the new value to the parent component
+					onInputChange(value)
+				}}
+			/>
+		</label>
+	)
+}
+
+//////////////////////////////////////
+
+const ExampleForm = ()=> {
+
+	// Use state to keep track of the value
+	const [inputValue, setInputValue] = useState('20:30') // default to 8:30 PM
+
+	return (
+		<form>
+			<TimeInput
+				label="Label text"
+
+				// Use the state value to set the time
+				currentValue={inputValue}
+
+				// Use the set state function to update the time when it changes
+				onInputChange={ newValue => setInputValue(newValue) }
+			/>
+			<button type="submit">Submit</button>
+		</form>
+	)
+}
+`}
 			/>
 
 			<ExampleBlock
@@ -59,9 +137,31 @@ function App() {
 						value={currentValue}
 						onChange={({ value }) => setValue(value)}
 						forcePolyfill={true}
-						className={className}
 					/>
 				)}
+				codeString={`
+import React from 'react'
+import TimeInputPolyfill from 'react-time-input-polyfill'
+
+const TimeInput = ({ label, currentValue, onInputChange }) => {
+	return (
+		<label>
+			<span>{label}</span>
+			<TimeInputPolyfill
+				value={currentValue}
+
+				/*  Force browsers that support input[type=time]
+					to use the polyfill.
+					(useful for testing and debugging)
+				*/  forcePolyfill={true}
+
+				onChange={({ value, element }) => {
+					onInputChange(value)
+				}
+			/>
+		</label>
+	)
+}`}
 			/>
 
 			<ExampleBlock
@@ -74,9 +174,30 @@ function App() {
 						className={className}
 					/>
 				)}
+				codeString={`
+// View this example in Internet Explorer
+// This is a normal time input with no polyfill applied
+
+import React from 'react'
+
+export const TimeInput = ({ label, currentValue, onInputChange }) => {
+	return (
+		<label>
+			<span>{label}</span>
+			<input
+				type="time"
+				value={currentValue}
+				onChange={(e) => {
+					console.log(e) // the default browser event
+					onInputChange(e.target.value)
+				}}
+			/>
+		</label>
+	)
+}`}
 			/>
 
-			{addedLater && (
+			{/* {addedLater && (
 				<ExampleBlock
 					label="Delayed forced polyfill"
 					Input={({ currentValue, setValue, className }) => (
@@ -88,7 +209,7 @@ function App() {
 						/>
 					)}
 				/>
-			)}
+			)} */}
 		</div>
 	)
 }
