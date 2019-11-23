18cc4e4d4a8e559fb0416f0aa4e5fbd42c48dedc
Daniel Tonon <dan503.npm@gmail.com>
2019-11-23 19:18:47 +1100

    Fix for the deletion of a single value deleting all three values

--- index.js
+++ index.js
@@ -114,7 +114,9 @@ export default class TimeInput extends React.Component {
 	set_time(time24hr) {
 		this.setState({
 			time: this.state.usePolyfill
-				? this.polyfill.get_values_from_24hr(time24hr || '')
+				? time24hr
+					? this.polyfill.get_values_from_24hr(time24hr)
+					: this.state.time
 				: null,
 			value24hr: time24hr,
 		})
