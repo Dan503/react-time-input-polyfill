266842e2c86bd79688db1024d24d551514e4b8f9
Daniel Tonon <dan503.npm@gmail.com>
2019-11-24 00:56:05 +1100

    Adding Docs folder back to master (I gave up on GitHub Actions)

---
+++ docs/precache-manifest.8534ea64c2dd5c2dd45813169414c739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e18e2b789944fbdaa7711a1a1444b8",
+    "url": "/react-time-input-polyfill/index.html"
+  },
+  {
+    "revision": "633b27d568cd0d59aa3c",
+    "url": "/react-time-input-polyfill/static/css/main.2743c8fd.chunk.css"
+  },
+  {
+    "revision": "4615afb6bcf76bf8b129",
+    "url": "/react-time-input-polyfill/static/js/2.c0466f14.chunk.js"
+  },
+  {
+    "revision": "633b27d568cd0d59aa3c",
+    "url": "/react-time-input-polyfill/static/js/main.7606c33a.chunk.js"
+  },
+  {
+    "revision": "47cf3a1f1fe1adf272ae",
+    "url": "/react-time-input-polyfill/static/js/runtime-main.c87944e3.js"
+  }
+]);
