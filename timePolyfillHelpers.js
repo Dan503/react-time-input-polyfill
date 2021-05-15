05ae751b2c82f0edb3e47c631d47505e6d810e80
Daniel Tonon <dan503.npm@gmail.com>
2021-05-15 21:59:46 +1000

    #16 - Converting timePolyfillHelpers to es module syntax
    The imports are es modules so it will be simplest if I convert this to es module syntax and generate the cjs friendly one

--- timePolyfillHelpers.js
+++ timePolyfillHelpers.js
@@ -1,44 +1,44 @@
-Element.prototype.matches =
-	Element.prototype.matches || Element.prototype.msMatchesSelector
-
 //static-values
-var segments = require('time-input-polyfill/core/static-values/segments')
+import segments from 'time-input-polyfill/core/static-values/segments'
 
 // converters
-var convert_to_12hr_time = require('time-input-polyfill/core/converters/convert_to_12hr_time')
-var convert_to_24hr_time = require('time-input-polyfill/core/converters/convert_to_24hr_time')
-var convert_hours_to_12hr_time = require('time-input-polyfill/core/converters/convert_hours_to_12hr_time')
+import convert_to_12hr_time from 'time-input-polyfill/core/converters/convert_to_12hr_time'
+import convert_to_24hr_time from 'time-input-polyfill/core/converters/convert_to_24hr_time'
+import convert_hours_to_12hr_time from 'time-input-polyfill/core/converters/convert_hours_to_12hr_time'
 
 //selectors
-var select_cursor_segment = require('time-input-polyfill/core/selectors/select_cursor_segment')
-var select_segment = require('time-input-polyfill/core/selectors/select_segment')
-var next_segment = require('time-input-polyfill/core/selectors/next_segment')
-var prev_segment = require('time-input-polyfill/core/selectors/prev_segment')
+import select_cursor_segment from 'time-input-polyfill/core/selectors/select_cursor_segment'
+import select_segment from 'time-input-polyfill/core/selectors/select_segment'
+import next_segment from 'time-input-polyfill/core/selectors/next_segment'
+import prev_segment from 'time-input-polyfill/core/selectors/prev_segment'
 
 // getters
-var get_label = require('time-input-polyfill/core/getters/get_label')
-var get_current_segment = require('time-input-polyfill/core/getters/get_current_segment')
-var get_values = require('time-input-polyfill/core/getters/get_values')
+import get_label from 'time-input-polyfill/core/getters/get_label'
+import get_current_segment from 'time-input-polyfill/core/getters/get_current_segment'
+import get_values from 'time-input-polyfill/core/getters/get_values'
+
+// a11y
+import create_a11y_block from 'time-input-polyfill/core/accessibility/create_a11y_block'
+import update_a11y from 'time-input-polyfill/core/accessibility/update_a11y'
+
+Element.prototype.matches =
+	Element.prototype.matches || Element.prototype.msMatchesSelector
 
 // manual_entry_log
 function manual_entry_log() {
 	this.items = []
 }
-manual_entry_log.prototype.clear = function() {
+manual_entry_log.prototype.clear = function () {
 	this.items = []
 }
-manual_entry_log.prototype.add = function(entry) {
+manual_entry_log.prototype.add = function (entry) {
 	this.items.push(parseInt(entry))
 }
 
-var get_values_from_24hr = value24hr => {
+var get_values_from_24hr = (value24hr) => {
 	const value12hr = convert_to_12hr_time(value24hr)
 	return get_values(null, value12hr)
 }
-
-// a11y
-var create_a11y_block = require('time-input-polyfill/core/accessibility/create_a11y_block')
-var update_a11y = require('time-input-polyfill/core/accessibility/update_a11y')
 
 window.timePolyfillHelpers = {
 	segments,
