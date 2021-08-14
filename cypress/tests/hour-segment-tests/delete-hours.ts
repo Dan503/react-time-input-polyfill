3fb12fb61158c92c8464d6b6f928ffe8ebae8207
Daniel Tonon <dan503.npm@gmail.com>
2021-08-14 14:19:02 +1000

    describe box around hours deletion tests

--- cypress/tests/hour-segment-tests/delete-hours.ts
+++ cypress/tests/hour-segment-tests/delete-hours.ts
@@ -3,16 +3,18 @@ import { loadTestPage } from "../../support/loadTestPage"
 import { cyInput } from "../../support/utils"
 
 export function deleteHours() {
-	it('Should clear hours on delete key press', () => {
-		loadTestPage().then(({ $input }) => {
-			selectSegment($input, 'hrs12')
-			cyInput().type('{del}').should('have.value', '--:30 PM')
+	describe('delete hours', () => {
+		it('Should clear hours on delete key press', () => {
+			loadTestPage().then(({ $input }) => {
+				selectSegment($input, 'hrs12')
+				cyInput().type('{del}').should('have.value', '--:30 PM')
+			})
 		})
-	})
-	it('Should clear hours on backspace key press', () => {
-		loadTestPage().then(({ $input }) => {
-			selectSegment($input, 'hrs12')
-			cyInput().type('{backspace}').should('have.value', '--:30 PM')
+		it('Should clear hours on backspace key press', () => {
+			loadTestPage().then(({ $input }) => {
+				selectSegment($input, 'hrs12')
+				cyInput().type('{backspace}').should('have.value', '--:30 PM')
+			})
 		})
 	})
 }
