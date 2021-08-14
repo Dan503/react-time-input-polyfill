8914b521bc4e11d9381958d2151dcf2362eb27ce
Daniel Tonon <dan503.npm@gmail.com>
2021-08-14 12:14:42 +1000

    Fixing failing miscellaneous test

--- cypress/integration/miscellaneous-tests.ts
+++ cypress/integration/miscellaneous-tests.ts
@@ -1,16 +1,15 @@
 /// <reference types="cypress" />
 
-import { loadTestPage, _forcedPolyfillId } from "../support/loadTestPage"
-import { selectSegment, toLeadingZero, getCursorSegment, selectCursorSegment } from '@time-input-polyfill/utils'
-import { Segment } from "@time-input-polyfill/utils/npm/types"
+import { _forcedPolyfillId } from "../support/utils"
 
 const cyVisit = () => cy.visit('http://localhost:3000/react-time-input-polyfill')
 const cyInput = () => cy.get(_forcedPolyfillId)
 
 describe('Miscellaneous tests', () => {
 	it('Should start with value of 08:30 PM', () => {
-		cyVisit()
-		cyInput().should('have.value', '08:30 PM')
+		cyVisit().then(() => {
+			cyInput().should('have.value', '08:30 PM')
+		})
 	})
 })
 
