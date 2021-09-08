fbe0906120a97d482b98df549fd484b8848b2d1c
Daniel Tonon <dan503.npm@gmail.com>
2021-09-08 22:18:40 +1000

    Setting up for a manual entry hours test

--- cypress/integration/hour-segment-tests.ts
+++ cypress/integration/hour-segment-tests.ts
@@ -1,10 +1,11 @@
-import { fromBlankHours, decrementHours, deleteHours, incrementHours } from "../tests/hour-segment-tests"
+import { fromBlankHours, decrementHours, deleteHours, incrementHours, manualEntryHours } from "../tests/hour-segment-tests"
 
 describe('hour segment', () => {
 	incrementHours()
 	decrementHours()
 	deleteHours()
 	fromBlankHours()
+	manualEntryHours()
 })
 
 export {}
---
+++ cypress/tests/hour-segment-tests/manual-entry-hours.ts
@@ -0,0 +1,8 @@
+import { loadTestPage } from "../../support"
+
+export function manualEntryHours() {
+	it('Should ____', () => {
+		loadTestPage({ segment: 'hrs12' }).then(() => {
+		})
+	})
+}
