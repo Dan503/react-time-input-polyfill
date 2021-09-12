ba77b562534de0ad2350f0b0eaaa9e095ea177c1
Daniel Tonon <dan503.npm@gmail.com>
2021-09-12 18:03:59 +1000

    File name spelling error fix

--- cypress/tests/manual-entry-tests/index.ts
+++ cypress/tests/manual-entry-tests/index.ts
@@ -1,4 +1,4 @@
 export * from './0-1_increments'
 export * from './2-5_increments'
 export * from './6-9_increments'
-export * from './other-menual-entry-tests'
+export * from './other-manual-entry-tests'
