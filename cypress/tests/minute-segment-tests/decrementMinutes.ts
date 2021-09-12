4467651c7aaaf5dda0ce6bbe8c24ee16333b9604
Daniel Tonon <dan503.npm@gmail.com>
2021-09-12 19:02:01 +1000

    Simplifying the increment and decrement minute loop tests
    Merging the tests into a single loop each instead of 2 seperate loops

--- cypress/tests/minute-segment-tests/decrementMinutes.ts
+++ cypress/tests/minute-segment-tests/decrementMinutes.ts
@@ -1,25 +1,24 @@
 import { toLeadingZero } from "@time-input-polyfill/utils"
-import { a11yHasExpectedHtml, hasReturnVal, loadTestPage, use } from "../../support"
+import { a11yHasExpectedHtml, hasReturnVal, loadTestPage, setTime, use } from "../../support"
 
 export function decrementMinutes() {
 	it('Should decrement as expected on down key press', () => {
 		loadTestPage({ segment: 'minutes' }).then(() => {
-			let a = 29
-			while (a > -1) {
+			setTime('12:00 AM', 'minutes').then(() => {
+				let a = 59
+				while (a >= 0) {
+					use.downArrow()
+						.should('have.value', `12:${toLeadingZero(a)} AM`)
+						.then(a11yHasExpectedHtml(`<p>${a}.</p>`))
+						.then(hasReturnVal(`00:${toLeadingZero(a)}`))
+					a--
+				}
+				// Testing that it loops back around at the end
 				use.downArrow()
-					.should('have.value', `08:${toLeadingZero(a)} PM`)
-					.then(a11yHasExpectedHtml(`<p>${a}.</p>`))
-					.then(hasReturnVal(`20:${toLeadingZero(a)}`))
-				a--
-			}
-			let b = 59
-			while (b > 1) {
-				use.downArrow()
-					.should('have.value', `08:${toLeadingZero(b)} PM`)
-					.then(a11yHasExpectedHtml(`<p>${b}.</p>`))
-					.then(hasReturnVal(`20:${toLeadingZero(b)}`))
-				b--
-			}
+					.should('have.value', `12:59 AM`)
+					.then(a11yHasExpectedHtml(`<p>59.</p>`))
+					.then(hasReturnVal(`00:59`))
+			})
 		})
 	})
 }
--- cypress/tests/minute-segment-tests/incrementMinutes.ts
+++ cypress/tests/minute-segment-tests/incrementMinutes.ts
@@ -1,19 +1,24 @@
 import { toLeadingZero } from "@time-input-polyfill/utils"
-import { a11yHasExpectedHtml, hasReturnVal, loadTestPage, use } from "../../support"
+import { a11yHasExpectedHtml, hasReturnVal, loadTestPage, setTime, use } from "../../support"
 
 export function incrementMinutes() {
 	it('Should increment as expected on up key press', () => {
 		loadTestPage({ segment: 'minutes' }).then(() => {
-			let a = 31
-			while (a < 60) {
-				use.upArrow().should('have.value', `08:${a} PM`).then(a11yHasExpectedHtml(`<p>${a}.</p>`)).then(hasReturnVal(`20:${a}`))
-				a++
-			}
-			let b = 0
-			while (b < 31) {
-				use.upArrow().should('have.value', `08:${toLeadingZero(b)} PM`).then(a11yHasExpectedHtml(`<p>${b}.</p>`)).then(hasReturnVal(`20:${toLeadingZero(b)}`))
-				b++
-			}
+			setTime('12:59 AM', 'minutes').then(() => {
+				let a = 0
+				while (a < 60) {
+					use.upArrow()
+						.should('have.value', `12:${toLeadingZero(a)} AM`)
+						.then(a11yHasExpectedHtml(`<p>${a}.</p>`))
+						.then(hasReturnVal(`00:${toLeadingZero(a)}`))
+					a++
+				}
+				// Testing that it loops back around at the end
+				use.upArrow()
+					.should('have.value', `12:00 AM`)
+					.then(a11yHasExpectedHtml(`<p>0.</p>`))
+					.then(hasReturnVal(`00:00`))
+			})
 		})
 	})
 }
