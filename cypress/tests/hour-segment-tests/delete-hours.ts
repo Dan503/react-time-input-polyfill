88a7a1bfba37cd418cfa232f87bdba23fded07cd
Daniel Tonon <dan503.npm@gmail.com>
2021-09-12 17:15:48 +1000

    Adding returnValue checks to all hour tests

--- cypress/tests/hour-segment-tests/delete-hours.ts
+++ cypress/tests/hour-segment-tests/delete-hours.ts
@@ -1,5 +1,5 @@
 import { loadTestPage } from "../../support/loadTestPage"
-import { a11yHasExpectedHtml, use } from "../../support/utils"
+import { a11yHasExpectedHtml, use, hasReturnVal } from "../../support/utils"
 
 export function deleteHours() {
 	describe('delete hours', () => {
@@ -8,12 +8,14 @@ export function deleteHours() {
 				.then(use.del)
 				.should('have.value', '--:30 PM')
 				.then(a11yHasExpectedHtml(`<p>blank.</p>`))
+				.then(hasReturnVal(''))
 		})
 		it('Should clear hours on backspace key press', () => {
 			loadTestPage({ segment: 'hrs12' })
 				.then(use.backspace)
 				.should('have.value', '--:30 PM')
 				.then(a11yHasExpectedHtml(`<p>blank.</p>`))
+				.then(hasReturnVal(''))
 		})
 	})
 }
--- cypress/tests/hour-segment-tests/from-blank-hours.ts
+++ cypress/tests/hour-segment-tests/from-blank-hours.ts
@@ -1,5 +1,5 @@
 import { loadTestPage } from "../../support/loadTestPage"
-import { clearAllSegments, use, a11yHasExpectedHtml } from "../../support/utils"
+import { clearAllSegments, use, a11yHasExpectedHtml, hasReturnVal } from "../../support/utils"
 
 export function fromBlankHours() {
 	describe('from blank hours', () => {
@@ -15,6 +15,7 @@ export function fromBlankHours() {
 						.then(use.upArrow)
 						.should('have.value', '01:30 PM')
 						.then(a11yHasExpectedHtml(`<p>1.</p>`))
+						.then(hasReturnVal('13:30'))
 				})
 
 				it('Should increment blank hours from --:-- -- to 01:-- --', () => {
@@ -23,6 +24,7 @@ export function fromBlankHours() {
 							.then(use.upArrow)
 							.should('have.value', '01:-- --')
 							.then(a11yHasExpectedHtml(`<p>1.</p>`))
+							.then(hasReturnVal(''))
 					)
 				})
 			})
@@ -37,6 +39,7 @@ export function fromBlankHours() {
 						.then(use.downArrow)
 						.should('have.value', '12:30 PM')
 						.then(a11yHasExpectedHtml(`<p>12.</p>`))
+						.then(hasReturnVal('12:30'))
 				})
 
 				it('Should decrement blank hours from --:-- -- to 12:-- --', () => {
@@ -45,6 +48,7 @@ export function fromBlankHours() {
 							.then(use.downArrow)
 							.should('have.value', '12:-- --')
 							.then(a11yHasExpectedHtml(`<p>12.</p>`))
+							.then(hasReturnVal(''))
 					)
 				})
 			})
