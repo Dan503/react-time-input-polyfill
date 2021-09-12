795191c66fc83e4bbb1abff06a2336452f9c9c62
Daniel Tonon <dan503.npm@gmail.com>
2021-09-12 19:12:26 +1000

    wrapping button tests in funcitons

--- cypress/integration/button-tests.ts
+++ cypress/integration/button-tests.ts
@@ -1,19 +1,29 @@
 import { cyInput, hasReturnVal, loadTestPage } from "../support"
 
 describe('Button tests', () => {
-	it('Button 1', () => {
-		loadTestPage().then(() => cy.get('#Forced-polyfill-button-1').click().then(() =>
-			cyInput().should('have.value', '07:15 AM').then(hasReturnVal('07:15'))
-		))
-	})
-	it('Button 2', () => {
-		loadTestPage().then(() => cy.get('#Forced-polyfill-button-2').click().then(() =>
-			cyInput().should('have.value', '03:45 PM').then(hasReturnVal('15:45'))
-		))
-	})
-	it('Button 3', () => {
-		loadTestPage().then(() => cy.get('#Forced-polyfill-button-3').click().then(() =>
-			cyInput().should('have.value', '--:-- --').then(hasReturnVal(''))
-		))
-	})
+	button_1()
+	button_2()
+	button_3()
+
+	function button_1() {
+		it('Button 1', () => {
+			loadTestPage().then(() => cy.get('#Forced-polyfill-button-1').click().then(() =>
+				cyInput().should('have.value', '07:15 AM').then(hasReturnVal('07:15'))
+			))
+		})
+	}
+	function button_2() {
+		it('Button 2', () => {
+			loadTestPage().then(() => cy.get('#Forced-polyfill-button-2').click().then(() =>
+				cyInput().should('have.value', '03:45 PM').then(hasReturnVal('15:45'))
+			))
+		})
+	}
+	function button_3() {
+		it('Button 3', () => {
+			loadTestPage().then(() => cy.get('#Forced-polyfill-button-3').click().then(() =>
+				cyInput().should('have.value', '--:-- --').then(hasReturnVal(''))
+			))
+		})
+	}
 })
