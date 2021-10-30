ba911b539788a59d50c8d364703f0c767be44d8f
Daniel Tonon <dan503.npm@gmail.com>
2021-10-30 14:41:21 +1100

    Adding export statements to make it easier to access the types

--- index.tsx
+++ index.tsx
@@ -1,1 +1,2 @@
 export { default } from './src/core/TimeInputPolyFill'
+export * from './src/core/TimeInputPolyFill'
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,2 @@
 export { default } from './TimeInputPolyFill'
+export * from './TimeInputPolyFill'
