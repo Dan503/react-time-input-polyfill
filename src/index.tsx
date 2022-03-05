615afc48427c5b91d9fe12bfd8218b3fc258eac0
Daniel Tonon <dan503.npm@gmail.com>
2022-03-05 20:09:42 +1100

    Adding replaceAll and repeat polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 
+// No where in my code (including `@time-input-polyfill/utils`) am I using `replaceAll` or `repeat`.
+// Something in my dependencies now requires these polyfills for IE11 to not crash
+import 'core-js/features/string/replace-all'
+import 'core-js/features/string/repeat'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
