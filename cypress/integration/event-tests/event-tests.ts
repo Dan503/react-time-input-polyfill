e69dd76cd9ca3bc927d96611674408bceb9749dd
Daniel Tonon <dan503.npm@gmail.com>
2023-06-18 21:49:39 +1000

    ignoring the form submit test, not necessary in React

--- cypress/integration/event-tests/event-tests.ts
+++ cypress/integration/event-tests/event-tests.ts
@@ -2,6 +2,7 @@ import { functionVsClassBased } from '../../support/functionVsClassBased'
 import { testSuite } from '../../support/testSuite'
 
 functionVsClassBased({
-	functionBased: testSuite.functionBased.tests.events.all,
-	classBased: testSuite.classBased.tests.events.all
+	// only testing inputEvents, we don't need to support form submit tests in React
+	functionBased: testSuite.functionBased.tests.events.inputEvents,
+	classBased: testSuite.classBased.tests.events.inputEvents
 })
